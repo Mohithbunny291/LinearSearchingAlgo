39152bdf0a4bc11c6e7915447faa374a7de1c405
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2024-01-09 15:24:25 +0530

    Changed to as at

--- SearchingAlgo/LinearSearch.cs
+++ SearchingAlgo/LinearSearch.cs
@@ -6,7 +6,7 @@ namespace LinearSearchingAlgo
     {
         public bool LinearSearchAlgo(int[] arr, int n)
         {
-            // Added to GitHub Web
+            // Added at GitHub Web
             Console.WriteLine("Linear Search");
             for (int i = 0; i < arr.Length; i++)
             {
