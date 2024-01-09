1437ad0467200b2c4ecc482dcd940ceac1185066
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2024-01-09 15:08:39 +0530

    Add project files.

---
+++ SearchingAlgo/LinearSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearchingAlgo
+{
+    internal class LinearSearch
+    {
+        public bool LinearSearchAlgo(int[] arr, int n)
+        {
+            Console.WriteLine("Linear Search");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == n)
+                {
+                    Console.WriteLine($"Element {n} found at index {i}");
+                    return true;
+                }
+            }return false;
+        }
+    }
+}
---
+++ SearchingAlgo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearchingAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number Of Elements");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Elements:");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the element to be searched");
+            int find = int.Parse(Console.ReadLine());
+
+            LinearSearch linearSearch = new LinearSearch();
+            if(linearSearch.LinearSearchAlgo(arr, find) == true)
+            {
+                Console.WriteLine("Search Complete");
+            }
+            else
+            {
+                Console.WriteLine("Element Not Found");
+            }
+        }
+    }
+}
